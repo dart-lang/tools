dart-archive/coverage

collect_coverage_test: delete temp directories when we're done

test/collect_coverage_test.dart
@@ -68,14 +68,18 @@ void main() {
   test('parseCoverage', () async {
     var tempDir = await Directory.systemTemp.createTemp('coverage.test.');
 
-    var outputFile = new File(p.join(tempDir.path, 'coverage.json'));
+    try {
+      var outputFile = new File(p.join(tempDir.path, 'coverage.json'));
 
-    var coverageResults = await getCoverageResult();
-    await outputFile.writeAsString(coverageResults, flush: true);
+      var coverageResults = await getCoverageResult();
+      await outputFile.writeAsString(coverageResults, flush: true);
 
-    var parsedResult = await parseCoverage([outputFile], 1);
+      var parsedResult = await parseCoverage([outputFile], 1);
 
-    expect(parsedResult, contains(_sampleAppFileUri));
-    expect(parsedResult, contains(_isolateLibFileUri));
+      expect(parsedResult, contains(_sampleAppFileUri));
+      expect(parsedResult, contains(_isolateLibFileUri));
+    } finally {
+      await tempDir.delete(recursive: true);
+    }
   });
 }

dart-lang/tools

pkgs/coverage/test/collect_coverage_test.dart
@@ -71,15 +71,19 @@ void main() {
   test('parseCoverage', () async {
     var tempDir = await Directory.systemTemp.createTemp('coverage.test.');
 
-    var outputFile = new File(p.join(tempDir.path, 'coverage.json'));
+    try {
+      var outputFile = new File(p.join(tempDir.path, 'coverage.json'));
 
-    var coverageResults = await _getCoverageResult();
-    await outputFile.writeAsString(coverageResults, flush: true);
+      var coverageResults = await _getCoverageResult();
+      await outputFile.writeAsString(coverageResults, flush: true);
 
-    var parsedResult = await parseCoverage([outputFile], 1);
+      var parsedResult = await parseCoverage([outputFile], 1);
 
-    expect(parsedResult, contains(_sampleAppFileUri));
-    expect(parsedResult, contains(_isolateLibFileUri));
+      expect(parsedResult, contains(_sampleAppFileUri));
+      expect(parsedResult, contains(_isolateLibFileUri));
+    } finally {
+      await tempDir.delete(recursive: true);
+    }
   });
 }
 