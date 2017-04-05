28618b3f1929fc63e325a64d306572c9ed6f61bd
Jacob MacDonald <jakemac@google.com>
2017-04-05 09:43:50 -0700

    add driver_test to test_all

--- pkgs/bazel_worker/test/test_all.dart
+++ pkgs/bazel_worker/test/test_all.dart
@@ -2,10 +2,12 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'driver_test.dart' as driver;
 import 'message_grouper_test.dart' as message_grouper;
 import 'worker_loop_test.dart' as worker_loop;
 
 void main() {
+  driver.main();
   message_grouper.main();
   worker_loop.main();
 }
