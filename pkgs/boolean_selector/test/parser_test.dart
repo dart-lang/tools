07ad19e12d6e7b11e1b67dbceb691f2614a76a65
Kevin Moore <kevmoo@users.noreply.github.com>
2021-04-23 10:54:53 -0700

    Update CI (dart-lang/boolean_selector#37)
    Use latest CI setup
    Sort directives
    Remove null-safety prerelease entries from changelog

--- pkgs/boolean_selector/test/parser_test.dart
+++ pkgs/boolean_selector/test/parser_test.dart
@@ -2,10 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:test/test.dart';
-
 import 'package:boolean_selector/src/ast.dart';
 import 'package:boolean_selector/src/parser.dart';
+import 'package:test/test.dart';
 
 /// A matcher that asserts that a value is a [ConditionalNode].
 final _isConditionalNode = TypeMatcher<ConditionalNode>();
