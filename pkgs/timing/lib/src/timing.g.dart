10424b0d7df9d8f7f5aa304732e1e6409373143f
Kevin Moore <kevmoo@users.noreply.github.com>
2021-07-15 10:30:10 -0700

    use latest deps, drop dep overrides (dart-lang/timing#18)

--- pkgs/timing/lib/src/timing.g.dart
+++ pkgs/timing/lib/src/timing.g.dart
@@ -6,25 +6,22 @@ part of 'timing.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-TimeSlice _$TimeSliceFromJson(Map<String, dynamic> json) {
-  return TimeSlice(
-    DateTime.parse(json['startTime'] as String),
-    DateTime.parse(json['stopTime'] as String),
-  );
-}
+TimeSlice _$TimeSliceFromJson(Map<String, dynamic> json) => TimeSlice(
+      DateTime.parse(json['startTime'] as String),
+      DateTime.parse(json['stopTime'] as String),
+    );
 
 Map<String, dynamic> _$TimeSliceToJson(TimeSlice instance) => <String, dynamic>{
       'startTime': instance.startTime.toIso8601String(),
       'stopTime': instance.stopTime.toIso8601String(),
     };
 
-TimeSliceGroup _$TimeSliceGroupFromJson(Map<String, dynamic> json) {
-  return TimeSliceGroup(
-    (json['slices'] as List<dynamic>)
-        .map((e) => TimeSlice.fromJson(e as Map<String, dynamic>))
-        .toList(),
-  );
-}
+TimeSliceGroup _$TimeSliceGroupFromJson(Map<String, dynamic> json) =>
+    TimeSliceGroup(
+      (json['slices'] as List<dynamic>)
+          .map((e) => TimeSlice.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
 
 Map<String, dynamic> _$TimeSliceGroupToJson(TimeSliceGroup instance) =>
     <String, dynamic>{
