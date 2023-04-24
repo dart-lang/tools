7644edd7b4d75e6cbdd41f061cf952b66ccc47a8
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-04-24 09:00:41 -0400

    Development began on survey handler class with fetch

---
+++ pkgs/unified_analytics/lib/src/survey_handler.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'constants.dart';
+
+class SurveyHandler {
+  /// Contains metadata for each survey hosted at the [kContextualSurveyUrl]
+  /// endpoint; if there are no surveys available, the list will remain empty
+  final List<Map<String, Object?>> surveyList = [];
+
+  SurveyHandler();
+
+  /// Retrieves the survey metadata file from [kContextualSurveyUrl]
+  Future<void> fetchSurveyList() async {
+    final Uri uri = Uri.parse(kContextualSurveyUrl);
+    final http.Response response = await http.get(uri);
+
+    // Parse the returned body and add to the list
+    print(utf8.decode(response.bodyBytes));
+    print(jsonDecode(utf8.decode(response.bodyBytes)));
+  }
+}
---
+++ pkgs/unified_analytics/test/survey_handler_test.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test/test.dart';
+
+import 'package:unified_analytics/unified_analytics.dart';
+
+void main() {
+  late SurveyHandler surveyHandler;
+
+  setUp(() {
+    surveyHandler = SurveyHandler();
+  });
+
+  test('survey tester', () async {
+    await surveyHandler.fetchSurveyList();
+  });
+}
