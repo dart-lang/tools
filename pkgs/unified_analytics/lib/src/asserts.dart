b14dc2ba9347cd9b5f0efcdc5eac6135ed5529d7
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-05-24 15:14:14 -0400

    Refactor to use custom `Exception` class

--- pkgs/unified_analytics/lib/src/asserts.dart
+++ pkgs/unified_analytics/lib/src/asserts.dart
@@ -6,12 +6,15 @@
 
 void checkBody(Map<String, Object?> body) {
   // Ensure we have the correct top level keys
-  assert(body.keys.contains('client_id') == true,
-      'client_id key not found in body of request');
-  assert(body.keys.contains('events') == true,
-      'events key not found in body of request');
-  assert(body.keys.contains('user_properties') == true,
-      'user_properties key not found in body of request');
+  if (!body.keys.contains('client_id')) {
+    throw AnalyticsException('client_id missing from top level keys');
+  }
+  if (!body.keys.contains('events')) {
+    throw AnalyticsException('events missing from top level keys');
+  }
+  if (!body.keys.contains('user_properties')) {
+    throw AnalyticsException('user_properties missing from top level keys');
+  }
 
   final List events = body['events'] as List;
   final Map<String, Object?> userProperties =
@@ -19,7 +22,9 @@ void checkBody(Map<String, Object?> body) {
 
   // GA4 Limitation:
   // Requests can have a maximum of 25 events
-  assert(events.length <= 25, 'Limit event params to 25 or less');
+  if (events.length > 25) {
+    throw AnalyticsException('25 is the max number of events');
+  }
 
   // Checks for each event object
   for (Map<String, Object?> eventMap in events) {
@@ -27,17 +32,22 @@ void checkBody(Map<String, Object?> body) {
     // Event names must be 40 characters or fewer, may only contain
     // alpha-numeric characters and underscores, and must start
     // with an alphabetic character
-    assert((eventMap['name'] as String).length <= 40,
-        'Limit event names to 40 chars or less');
-    assert(RegExp(r'^[A-Za-z0-9_]+$').hasMatch(eventMap['name'] as String),
-        'Event name can only have alphanumeric chars and underscores');
-    assert(RegExp(r'^[A-Za-z]+$').hasMatch((eventMap['name'] as String)[0]),
-        'Event name first char must be alphabetic char');
+    if ((eventMap['name'] as String).length > 40) {
+      throw AnalyticsException('Limit event names to 40 chars or less');
+    }
+    if (!RegExp(r'^[A-Za-z0-9_]+$').hasMatch(eventMap['name'] as String)) {
+      throw AnalyticsException(
+          'Event name can only have alphanumeric chars and underscores');
+    }
+    if (!RegExp(r'^[A-Za-z]+$').hasMatch((eventMap['name'] as String)[0])) {
+      throw AnalyticsException('Event name first char must be alphabetic char');
+    }
 
     // GA4 Limitation:
     // Events can have a maximum of 25 parameters
-    assert((eventMap['params'] as Map<String, Object?>).length <= 25,
-        'Limit params for each event to less than 25');
+    if ((eventMap['params'] as Map<String, Object?>).length > 25) {
+      throw AnalyticsException('Limit params for each event to less than 25');
+    }
 
     // Loop through each of the event parameters
     (eventMap['params'] as Map<String, Object?>).forEach((key, value) {
@@ -45,34 +55,48 @@ void checkBody(Map<String, Object?> body) {
       // Ensure that each value for the event params is one
       // of the following types:
       // `String`, `int`, `double`, or `bool`
-      assert(
-          value is String || value is int || value is double || value is bool,
-          'Values for event params have to be String, int, double, or bool');
+      if (!(value is String ||
+          value is int ||
+          value is double ||
+          value is bool)) {
+        throw AnalyticsException(
+            'Values for event params have to be String, int, double, or bool');
+      }
 
       // GA4 Limitation:
       // Parameter names (including item parameters) must be 40 characters
       // or fewer, may only contain alpha-numeric characters and underscores,
       // and must start with an alphabetic character
-      assert(key.length <= 40, 'Limit event param names to 40 chars or less');
-      assert(RegExp(r'^[A-Za-z0-9_]+$').hasMatch(key),
-          'Event param name can only have alphanumeric chars and underscores');
-      assert(RegExp(r'^[A-Za-z]+$').hasMatch(key[0]),
-          'Event param name first char must be alphabetic char');
+      if (key.length > 40) {
+        throw AnalyticsException('Limit event param names to 40 chars or less');
+      }
+      if (!RegExp(r'^[A-Za-z0-9_]+$').hasMatch(key)) {
+        throw AnalyticsException(
+            'Event param name can only have alphanumeric chars and underscores');
+      }
+      if (!RegExp(r'^[A-Za-z]+$').hasMatch(key[0])) {
+        throw AnalyticsException(
+            'Event param name first char must be alphabetic char');
+      }
 
       // GA4 Limitation:
       // Parameter values (including item parameter values) must be 100
       // characters or fewer
       if (value.runtimeType == String) {
         value as String;
-        assert(value.length <= 100,
-            'Limit characters in event param value to 100 chars or less');
+        if (value.length > 100) {
+          throw AnalyticsException(
+              'Limit characters in event param value to 100 chars or less');
+        }
       }
     });
   }
 
   // GA4 Limitation:
   // Events can have a maximum of 25 user properties
-  assert(userProperties.length <= 25, 'Limit user properties to 25 or less');
+  if (userProperties.length > 25) {
+    throw AnalyticsException('Limit user properties to 25 or less');
+  }
 
   // Checks for each user property item
   userProperties.forEach((key, value) {
@@ -80,13 +104,23 @@ void checkBody(Map<String, Object?> body) {
 
     // GA4 Limitation:
     // User property names must be 24 characters or fewer
-    assert(key.length <= 24, 'Limit user property names to 24 chars or less');
+    if (key.length > 24) {
+      throw AnalyticsException('Limit user property names to 24 chars or less');
+    }
 
     // GA4 Limitation:
     // User property values must be 36 characters or fewer
     if (value['value'].runtimeType == String) {
-      assert((value['value'] as String).length <= 36,
-          'Limit user property values to 36 chars or less');
+      if ((value['value'] as String).length > 36) {
+        throw AnalyticsException(
+            'Limit user property values to 36 chars or less');
+      }
     }
   });
+}
+
+class AnalyticsException implements Exception {
+  final String message;
+
+  AnalyticsException(this.message);
 }
--- pkgs/unified_analytics/test/asserts_test.dart
+++ pkgs/unified_analytics/test/asserts_test.dart
@@ -10,6 +10,361 @@ void main() {
   test('Failure if client_id top level key is missing', () {
     final Map<String, Object?> body = {};
 
-    expect(() => checkBody(body), throwsA(isA<AssertionError>()));
+    final String expectedErrorMessage = 'client_id missing from top level keys';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure if events top level key is missing', () {
+    final Map<String, Object?> body = {'client_id': 'xxxxxxx'};
+
+    final String expectedErrorMessage = 'events missing from top level keys';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure if user_properties top level key is missing', () {
+    final Map<String, Object?> body = {'client_id': 'xxxxxxx', 'events': []};
+
+    final String expectedErrorMessage =
+        'user_properties missing from top level keys';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure if more than 25 events found in events list', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[],
+      'user_properties': <String, Object?>{}
+    };
+
+    // Add more than the 25 allowed events
+    for (int i = 0; i < 30; i++) {
+      (body['events'] as List).add({'name': i});
+    }
+
+    final String expectedErrorMessage = '25 is the max number of events';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when event name is greater than 40 chars', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name':
+              'hot_reload_timehot_reload_timehot_reload_timehot_reload_time',
+          'params': {'time_ms': 133, 'count': 1999000}
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final String expectedErrorMessage = 'Limit event names to 40 chars or less';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when event name has invalid chars', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time!!',
+          'params': {'time_ms': 133, 'count': 1999000}
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final String expectedErrorMessage =
+        'Event name can only have alphanumeric chars and underscores';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when event name does not start with alphabetic char', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': '2hot_reload_time',
+          'params': {'time_ms': 133, 'count': 1999000}
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final String expectedErrorMessage =
+        'Event name first char must be alphabetic char';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when an event has more than 25 event params', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          // 'params': {...} SUBBING THIS VALUE OUT 30 Maps
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final Map<String, Object?> params = {};
+    for (int i = 0; i < 30; i++) {
+      params['$i'] = i;
+    }
+
+    // Add the params to the first event in the body
+    (body['events'] as List).first['params'] = params;
+
+    final String expectedErrorMessage =
+        'Limit params for each event to less than 25';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when an value for event params is not a supported type (list)',
+      () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          'params': {
+            'time_ms': 133,
+            // Lists and Maps are not supported
+            'count': <int>[1999000],
+          }
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final String expectedErrorMessage =
+        'Values for event params have to be String, int, double, or bool';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when an value for event params is not a supported type (map)',
+      () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          'params': {
+            'time_ms': 133,
+            // Lists and Maps are not supported
+            'count': <int, int>{5: 20},
+          }
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final String expectedErrorMessage =
+        'Values for event params have to be String, int, double, or bool';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when event param name is more than 40 chars', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          'params': {'time_mstime_mstime_mstime_mstime_mstime_ms': 133}
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final String expectedErrorMessage =
+        'Limit event param names to 40 chars or less';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure for event param name that has invalid chars', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          'params': {'time_ns!': 133}
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final String expectedErrorMessage =
+        'Event param name can only have alphanumeric chars and underscores';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test(
+      'Failure for event param name that does not start with an alphabetic char',
+      () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          'params': {'22time_ns': 133}
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final String expectedErrorMessage =
+        'Event param name first char must be alphabetic char';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure for event param values that are greater than 100 chars', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          'params': {
+            'time_ns': 'dsfjlksdjfajlfdsfjlks'
+                'djfajlfdsfjlksdjfajlfdsfjlksdjfaj'
+                'lfdsfjlksdjfajlfdsfjlksdjfajlfdsf'
+                'jlksdjfajlfdsfjlksdjfajlf'
+          }
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    final String expectedErrorMessage =
+        'Limit characters in event param value to 100 chars or less';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when body has more than 25 user properties', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          'params': {'time_ns': 123}
+        }
+      ],
+      'user_properties': <String, Object?>{}
+    };
+
+    for (int i = 0; i < 30; i++) {
+      (body['user_properties']! as Map<String, Object?>)['$i'] = i;
+    }
+
+    final String expectedErrorMessage = 'Limit user properties to 25 or less';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when user properties names are greater than 24 chars', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          'params': {'time_ns': 123}
+        }
+      ],
+      'user_properties': <String, Object?>{
+        'testtesttesttesttesttesttest': <String, Object?>{}, // TOO LONG
+      }
+    };
+
+    final String expectedErrorMessage =
+        'Limit user property names to 24 chars or less';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
+  });
+
+  test('Failure when user properties values are greater than 36 chars', () {
+    final Map<String, Object?> body = {
+      'client_id': 'xxxxxxx',
+      'events': <Map<String, Object?>>[
+        {
+          'name': 'hot_reload_time',
+          'params': {'time_ns': 123}
+        }
+      ],
+      'user_properties': <String, Object?>{
+        'test': <String, Object?>{
+          'value': 'testtesttesttesttesttesttesttesttesttest' // TOO LONG
+        },
+      }
+    };
+
+    final String expectedErrorMessage =
+        'Limit user property values to 36 chars or less';
+    expect(
+        () => checkBody(body),
+        throwsA(predicate(
+            (AnalyticsException e) => e.message == expectedErrorMessage,
+            expectedErrorMessage)));
   });
 }
