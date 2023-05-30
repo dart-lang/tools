c67524096be4c8933ebda0b068d9d988c105da8a
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-05-30 13:35:15 -0400

    Successful test added

--- pkgs/unified_analytics/test/asserts_test.dart
+++ pkgs/unified_analytics/test/asserts_test.dart
@@ -367,4 +367,27 @@ void main() {
             (AnalyticsException e) => e.message == expectedErrorMessage,
             expectedErrorMessage)));
   });
+
+  test('Successful body passes all asserts', () {
+    final Map<String, Object?> body = {
+      'client_id': '46cc0ba6-f604-4fd9-aa2f-8a20beb24cd4',
+      'events': [
+        {
+          'name': 'testing',
+          'params': {'time_ns': 345}
+        }
+      ],
+      'user_properties': {
+        'session_id': {'value': 1673466750423},
+        'flutter_channel': {'value': 'ey-test-channel'},
+        'host': {'value': 'macos'},
+        'flutter_version': {'value': 'Flutter 3.6.0-7.0.pre.47'},
+        'dart_version': {'value': 'Dart 2.19.0'},
+        'tool': {'value': 'flutter-tools'},
+        'local_time': {'value': '2023-01-11 14:53:31.471816 -0500'}
+      }
+    };
+
+    expect(() => checkBody(body), isA<void>());
+  });
 }
