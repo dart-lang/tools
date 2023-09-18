fb635d267b0862dc61fa685650b5bd901236c4d6
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-09-18 10:55:59 -0400

    Sort results from validDashTools getter

--- pkgs/unified_analytics/lib/src/enums.dart
+++ pkgs/unified_analytics/lib/src/enums.dart
@@ -2,8 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// The valid dash tool labels stored in the [DashTool] enum
-Set<String> get validDashTools => DashTool.values.map((e) => e.label).toSet();
+/// The valid dash tool labels stored in the [DashTool] enum.
+List<String> get validDashTools =>
+    DashTool.values.map((e) => e.label).toList()..sort();
 
 /// Values for the event name to be sent to Google Analytics.
 ///
