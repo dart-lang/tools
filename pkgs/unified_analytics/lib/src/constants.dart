6de56af82d6843a939f6437ddbe9cfcca1c675c4
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-09-26 15:36:48 -0400

    Update constants.dart

--- pkgs/unified_analytics/lib/src/constants.dart
+++ pkgs/unified_analytics/lib/src/constants.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// The hard coded client ID for the [NoOpAnalytics]
+/// The hard coded client ID for the [NoOpAnalytics] instance.
 const String clientIdNoOp = 'xxxx-xxxx';
 
 /// URL endpoint for sending Google Analytics Events.
