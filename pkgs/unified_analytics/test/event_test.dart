4dd3182b53dc76b0fcc7c7a87d74bcba5e9e5258
eliasyishak <42216813+eliasyishak@users.noreply.github.com>
2023-10-19 12:46:23 -0400

    Additional checks on `eventData` fields

--- pkgs/unified_analytics/test/event_test.dart
+++ pkgs/unified_analytics/test/event_test.dart
@@ -3,18 +3,28 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:test/test.dart';
+
+import 'package:unified_analytics/src/enums.dart';
 import 'package:unified_analytics/unified_analytics.dart';
 
 void main() {
   test('Event.doctorValidatorResult constructed', () {
-    expect(
-        () => Event.doctorValidatorResult(
-              validatorName: 'validatorName',
-              result: 'result',
-              partOfGroupedValidator: false,
-              doctorInvocationId: 123,
-              statusInfo: 'statusInfo',
-            ),
-        returnsNormally);
+    Event genDoctorValidatorResult() => Event.doctorValidatorResult(
+          validatorName: 'validatorName',
+          result: 'success',
+          partOfGroupedValidator: false,
+          doctorInvocationId: 123,
+          statusInfo: 'statusInfo',
+        );
+
+    final constructedEvent = genDoctorValidatorResult();
+
+    expect(genDoctorValidatorResult, returnsNormally);
+    expect(constructedEvent.eventName, DashEvent.doctorValidatorResult);
+    expect(constructedEvent.eventData['validatorName'], 'validatorName');
+    expect(constructedEvent.eventData['result'], 'success');
+    expect(constructedEvent.eventData['partOfGroupedValidator'], false);
+    expect(constructedEvent.eventData['doctorInvocationId'], 123);
+    expect(constructedEvent.eventData['statusInfo'], 'statusInfo');
   });
 }
