2d58436eefdc13d85fa5f7db0d2f59411274db7b
Kevin Moore <kevmoo@users.noreply.github.com>
2024-04-10 13:26:12 -0700

    Bump to latest lints (dart-lang/timing#42)

--- pkgs/timing/test/timing_test.dart
+++ pkgs/timing/test/timing_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: only_throw_errors
+// ignore_for_file: only_throw_errors, inference_failure_on_instance_creation
 
 import 'dart:async';
 
