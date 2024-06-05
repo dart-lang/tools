820b0163826b7dff2455dfaf4ec1c248b5b75173
Andrew Kolos <andrewrkolos@gmail.com>
2024-06-05 00:48:48 -0700

    format

--- pkgs/unified_analytics/test/log_handler_test.dart
+++ pkgs/unified_analytics/test/log_handler_test.dart
@@ -193,25 +193,24 @@ void main() {
   });
 
   test(
-    'Catches and discards any FileSystemException raised from attempting to write to the log file',
-    () async {
-      final logFilePath = 'log.txt';
-      final fs = MemoryFileSystem.test(opHandle: (context, operation) {
-        if (context == logFilePath && operation == FileSystemOp.write) {
-          throw FileSystemException(
-            'writeFrom failed',
-            logFilePath,
-            OSError('No space left on device', 28),
-          );
-        }
-      });
-      final logFile = fs.file(logFilePath);
-      logFile.createSync();
-      final logHandler = LogHandler(logFile: logFile);
+      'Catches and discards any FileSystemException raised from attempting '
+      'to write to the log file', () async {
+    final logFilePath = 'log.txt';
+    final fs = MemoryFileSystem.test(opHandle: (context, operation) {
+      if (context == logFilePath && operation == FileSystemOp.write) {
+        throw FileSystemException(
+          'writeFrom failed',
+          logFilePath,
+          OSError('No space left on device', 28),
+        );
+      }
+    });
+    final logFile = fs.file(logFilePath);
+    logFile.createSync();
+    final logHandler = LogHandler(logFile: logFile);
 
-      logHandler.save(data: {});
-    },
-  );
+    logHandler.save(data: {});
+  });
 
   test('Catching cast errors for each log record silently', () async {
     // Write a json array to the log file which will cause
