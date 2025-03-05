6faeacaa64d2c583bf1a4190d8eea151399ef1fc
Nick Nevzorov <62462330+NickNevzorov@users.noreply.github.com>
2025-03-05 10:26:25 +0200

    Update mime.dart

--- pkgs/mime/lib/mime.dart
+++ pkgs/mime/lib/mime.dart
@@ -10,6 +10,7 @@
 library;
 
 export 'src/extension.dart';
+export 'src/default_extension.dart';
 export 'src/mime_multipart_transformer.dart';
 export 'src/mime_shared.dart';
 export 'src/mime_type.dart';
