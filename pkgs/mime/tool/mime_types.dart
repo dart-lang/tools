d03ab79e8ec9ca0a4116c9e10590c3502f23240a
Devon Carew <devoncarew@gmail.com>
2025-03-13 15:58:56 -0700

    [package:mime] generate a markdown table of the current mime mappings

---
+++ pkgs/mime/tool/mime_types.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:mime/mime.dart';
+import 'package:mime/src/default_extension_map.dart';
+
+void main(List<String> args) {
+  // Get all the mime types.
+  final mimeTypes = defaultExtensionMap.values.toSet().toList()..sort();
+
+  // Find all their extensions.
+  final mimeToExts = <String, List<String>>{};
+
+  for (final entry in defaultExtensionMap.entries) {
+    final ext = entry.key;
+    final mime = entry.value;
+
+    mimeToExts.putIfAbsent(mime, () => []).add(ext);
+  }
+
+  // Emit the table.
+  const marker = '<!-- table -->\n';
+  final file = File('doc/mime_types.md');
+  var contents = file.readAsStringSync();
+  final prefix =
+      contents.substring(0, contents.indexOf(marker) + marker.length);
+  final suffix = contents.substring(contents.lastIndexOf(marker));
+
+  final buf = StringBuffer();
+  buf.write('''
+| MIME type | Default ext | Additional exts |
+| - | - | - |
+''');
+
+  for (final mime in mimeTypes) {
+    final defaultExt = extensionFromMime(mime)!;
+    final exts = mimeToExts[mime]!;
+
+    exts.remove(defaultExt);
+    exts.sort();
+
+    final decribeMime = '`$mime`';
+    final decribeExt = '`$defaultExt`';
+    final describeExts = exts.map((ext) => '`$ext`').join(', ');
+
+    buf.writeln('| $decribeMime | $decribeExt | $describeExts |');
+  }
+
+  contents = '$prefix\n$buf\n$suffix';
+  file.writeAsStringSync(contents);
+}
+
+String min(String str, [int width = 12]) => str.padRight(width);
