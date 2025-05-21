46797694d5f3bab5428da6e7893d8612cd35a668
Liam Appelbe <liama@google.com>
2025-05-21 12:41:18 +1000

    Increate test timeout

--- pkgs/coverage/test/test_util.dart
+++ pkgs/coverage/test/test_util.dart
@@ -11,7 +11,7 @@ import 'package:test_process/test_process.dart';
 
 final String testAppPath = p.join('test', 'test_files', 'test_app.dart');
 
-const Duration timeout = Duration(seconds: 20);
+const Duration timeout = Duration(seconds: 30);
 
 Future<TestProcess> runTestApp() => TestProcess.start(
       Platform.resolvedExecutable,
