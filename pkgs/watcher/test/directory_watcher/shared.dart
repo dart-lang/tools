c39a2377061c90cefd246d30ed2dee5706c5a696
Vyacheslav Egorov <vegorov@google.com>
2025-09-30 10:16:13 +0200

    Fix import order

--- pkgs/watcher/test/directory_watcher/shared.dart
+++ pkgs/watcher/test/directory_watcher/shared.dart
@@ -1,8 +1,8 @@
 // Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-import 'dart:isolate';
 import 'dart:io' as io;
+import 'dart:isolate';
 
 import 'package:test/test.dart';
 import 'package:test_descriptor/test_descriptor.dart' as d;
