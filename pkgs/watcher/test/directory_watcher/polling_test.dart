5db517650e23eed8d541d2aab2f8ea6b42c87b38
David Morgan <davidmorgan@google.com>
2025-10-06 13:41:30 +0200

    Allow more time for tests on Windows.

--- pkgs/watcher/test/directory_watcher/polling_test.dart
+++ pkgs/watcher/test/directory_watcher/polling_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@Timeout.factor(2)
+library;
+
 import 'package:test/test.dart';
 import 'package:watcher/watcher.dart';
 
