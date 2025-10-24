67bd72179bc4f1f0d7d542f32a65777de19ec62d
David Morgan <davidmorgan@google.com>
2025-10-24 12:16:04 +0200

    Cleanup.

--- pkgs/watcher/lib/src/directory_watcher/windows_isolate_directory_watcher.dart
+++ pkgs/watcher/lib/src/directory_watcher/windows_isolate_directory_watcher.dart
@@ -9,38 +9,53 @@ import '../resubscribable.dart';
 import '../watch_event.dart';
 import 'windows_manually_closed_directory_watcher.dart';
 
+/// Runs [WindowsManuallyClosedDirectoryWatcher] in an isolate to work around
+/// a platform limitation.
+///
+/// On Windows, Directory.watch fails if too many events arrive without being
+/// processed by the Dart VM. See `directory_watcher/windows_test.dart` for code
+/// that reliably triggers the failure by doing file writes in a synchronous
+/// block that prevents the Dart VM from processing the events caused.
+///
+/// Running the watcher in an isolate makes buffer exhaustion much less likely
+/// as there is no unrelated work happening in the isolate that would block
+/// processing of events.
 class WindowsIsolateDirectoryWatcher implements ManuallyClosedWatcher {
   @override
   final String path;
   final ReceivePort _receivePort = ReceivePort();
   SendPort? _sendPort;
 
-// TODO(davidmorgan): broadcast?
-  final StreamController<WatchEvent> _eventsController = StreamController();
+  final StreamController<WatchEvent> _eventsController =
+      StreamController.broadcast();
   final Completer<void> _readyCompleter = Completer();
 
   WindowsIsolateDirectoryWatcher(this.path) {
     _startIsolate(path, _receivePort.sendPort);
-    _receivePort.listen((event) {
-      if (_sendPort == null) {
-        _sendPort = event as SendPort;
-        return;
-      } else if (event == 'ready') {
-        _readyCompleter.complete();
-        return;
-      } else if (event is List) {
-        for (var i = 0; i != event.length; i += 2) {
-          final type = _parseChangeType(event[i] as int);
-          final path = event[i + 1] as String;
-          _eventsController.add(WatchEvent(type, path));
-        }
-      }
-    });
+    _receivePort
+        .listen((event) => _receiveFromIsolate(_Event.fromObject(event)));
+  }
+
+  void _receiveFromIsolate(_Event event) {
+    if (event.isSendPort()) {
+      _sendPort = event.sendPort;
+      return;
+    }
+    if (event.isReady()) {
+      _readyCompleter.complete();
+      return;
+    }
+    if (event.isWatchEvent()) {
+      _eventsController.add(event.watchEvent);
+      return;
+    }
   }
 
   @override
   void close() {
-    _sendPort!.send('close');
+    // The "close" event is the only event sent to the isolate, just send
+    // `null`.
+    _sendPort!.send(null);
   }
 
   @override
@@ -70,48 +85,67 @@ class _WatcherIsolate {
   _WatcherIsolate(this.path, this.sendPort)
       : watcher = WindowsManuallyClosedDirectoryWatcher(path) {
     final receivePort = ReceivePort();
-    sendPort.send(receivePort.sendPort);
 
-    watcher.events.listen((event) {
-      sendPort.send([event.type.index, event.path]);
-    });
+    // Three types of event are sent to the host.
+
+    // `_Event.sendPort` on startup.
+    sendPort.send(_Event.sendPort(receivePort.sendPort));
+
+    // `_Event.ready` when ready.
     watcher.ready.then((_) {
-      sendPort.send('ready');
+      sendPort.send(_Event.ready());
+    });
+
+    // `_Event.watchEvent` on any event.
+    // TODO: what about errors?
+    watcher.events.listen((event) {
+      sendPort.send(_Event.watchEvent(event));
     });
 
     receivePort.listen((event) {
-      if (event == 'close') {
-        watcher.close();
-        _closeCompleter.complete();
-      }
+      // The only event sent from the host to the isolate is "close".
+      watcher.close();
+      _closeCompleter.complete();
     });
   }
 }
 
-ChangeType _parseChangeType(int type) {
-  switch (type) {
-    case 0:
-      return ChangeType.ADD;
-    case 1:
-      return ChangeType.MODIFY;
-    case 2:
-      return ChangeType.REMOVE;
-    default:
-      throw ArgumentError.value(type, 'type');
+/// Event sent from the isolate to the host.
+extension type _Event._(Object? _object) {
+  factory _Event.sendPort(SendPort sendPort) => _Event._(sendPort);
+  factory _Event.ready() => _Event._(null);
+  factory _Event.watchEvent(WatchEvent event) {
+    final typeNumber = switch (event.type) {
+      ChangeType.ADD => 0,
+      ChangeType.MODIFY => 1,
+      ChangeType.REMOVE => 2,
+      _ => throw ArgumentError.value(event.type),
+    };
+    return _Event._([typeNumber, event.path]);
   }
-}
 
-extension ChangeTypeExtensions on ChangeType {
-  int get index {
-    switch (this) {
-      case ChangeType.ADD:
-        return 0;
-      case ChangeType.MODIFY:
-        return 1;
-      case ChangeType.REMOVE:
-        return 2;
-      default:
-        throw ArgumentError.value(this);
-    }
+  factory _Event.fromObject(Object? object) {
+    if (object == null) return _Event.ready();
+    if (object is List || object is SendPort) return _Event._(object);
+    throw ArgumentError.value(object, 'object');
+  }
+
+  bool isSendPort() => _object is SendPort;
+  bool isReady() => _object == null;
+  bool isWatchEvent() => _object is List;
+
+  SendPort get sendPort => _object as SendPort;
+
+  WatchEvent get watchEvent {
+    _object as List;
+    final typeNumber = _object[0] as int;
+    final type = switch (typeNumber) {
+      0 => ChangeType.ADD,
+      1 => ChangeType.MODIFY,
+      2 => ChangeType.REMOVE,
+      _ => throw ArgumentError.value(typeNumber, 'type'),
+    };
+    final path = _object[1] as String;
+    return WatchEvent(type, path);
   }
 }
