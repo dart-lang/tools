2deba0c6ed776124731b07124675e245db8bb914
Ömer Ağacan <omersa@google.com>
2025-11-06 08:57:57 +0000

    Update bazel_worker protos with protoc_plugin-25.0.0

--- pkgs/bazel_worker/lib/src/worker_protocol.pb.dart
+++ pkgs/bazel_worker/lib/src/worker_protocol.pb.dart
@@ -1,17 +1,20 @@
+// This is a generated file - do not edit.
 //
-//  Generated code. Do not modify.
-//  source: worker_protocol.proto
-//
-// @dart = 2.12
+// Generated from worker_protocol.proto.
+
+// @dart = 3.3
 
 // ignore_for_file: annotate_overrides, camel_case_types, comment_references
-// ignore_for_file: constant_identifier_names, library_prefixes
-// ignore_for_file: non_constant_identifier_names, prefer_final_fields
-// ignore_for_file: unnecessary_import, unnecessary_this, unused_import
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: curly_braces_in_flow_control_structures
+// ignore_for_file: deprecated_member_use_from_same_package, library_prefixes
+// ignore_for_file: non_constant_identifier_names
 
 import 'dart:core' as $core;
 
 import 'package:protobuf/protobuf.dart' as $pb;
+
+export 'package:protobuf/protobuf.dart' show GeneratedMessageGenericExtensions;
 
 /// An input file.
 class Input extends $pb.GeneratedMessage {
@@ -19,22 +22,20 @@ class Input extends $pb.GeneratedMessage {
     $core.String? path,
     $core.List<$core.int>? digest,
   }) {
-    final $result = create();
-    if (path != null) {
-      $result.path = path;
-    }
-    if (digest != null) {
-      $result.digest = digest;
-    }
-    return $result;
+    final result = create();
+    if (path != null) result.path = path;
+    if (digest != null) result.digest = digest;
+    return result;
   }
-  Input._() : super();
-  factory Input.fromBuffer($core.List<$core.int> i,
-          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromBuffer(i, r);
-  factory Input.fromJson($core.String i,
-          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromJson(i, r);
+
+  Input._();
+
+  factory Input.fromBuffer($core.List<$core.int> data,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(data, registry);
+  factory Input.fromJson($core.String json,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(json, registry);
 
   static final $pb.BuilderInfo _i = $pb.BuilderInfo(
       _omitMessageNames ? '' : 'Input',
@@ -45,22 +46,19 @@ class Input extends $pb.GeneratedMessage {
         2, _omitFieldNames ? '' : 'digest', $pb.PbFieldType.OY)
     ..hasRequiredFields = false;
 
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-      'Will be removed in next major version')
-  Input clone() => Input()..mergeFromMessage(this);
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-      'Will be removed in next major version')
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
+  Input clone() => deepCopy();
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
   Input copyWith(void Function(Input) updates) =>
       super.copyWith((message) => updates(message as Input)) as Input;
 
+  @$core.override
   $pb.BuilderInfo get info_ => _i;
 
   @$core.pragma('dart2js:noInline')
   static Input create() => Input._();
+  @$core.override
   Input createEmptyInstance() => create();
-  static $pb.PbList<Input> createRepeated() => $pb.PbList<Input>();
   @$core.pragma('dart2js:noInline')
   static Input getDefault() =>
       _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Input>(create);
@@ -73,14 +71,11 @@ class Input extends $pb.GeneratedMessage {
   @$pb.TagNumber(1)
   $core.String get path => $_getSZ(0);
   @$pb.TagNumber(1)
-  set path($core.String v) {
-    $_setString(0, v);
-  }
-
+  set path($core.String value) => $_setString(0, value);
   @$pb.TagNumber(1)
   $core.bool hasPath() => $_has(0);
   @$pb.TagNumber(1)
-  void clearPath() => clearField(1);
+  void clearPath() => $_clearField(1);
 
   /// A hash-value of the contents. The format of the contents is unspecified and
   /// the digest should be treated as an opaque token. This can be empty in some
@@ -88,14 +83,11 @@ class Input extends $pb.GeneratedMessage {
   @$pb.TagNumber(2)
   $core.List<$core.int> get digest => $_getN(1);
   @$pb.TagNumber(2)
-  set digest($core.List<$core.int> v) {
-    $_setBytes(1, v);
-  }
-
+  set digest($core.List<$core.int> value) => $_setBytes(1, value);
   @$pb.TagNumber(2)
   $core.bool hasDigest() => $_has(1);
   @$pb.TagNumber(2)
-  void clearDigest() => clearField(2);
+  void clearDigest() => $_clearField(2);
 }
 
 /// This represents a single work unit that Blaze sends to the worker.
@@ -108,77 +100,63 @@ class WorkRequest extends $pb.GeneratedMessage {
     $core.int? verbosity,
     $core.String? sandboxDir,
   }) {
-    final $result = create();
-    if (arguments != null) {
-      $result.arguments.addAll(arguments);
-    }
-    if (inputs != null) {
-      $result.inputs.addAll(inputs);
-    }
-    if (requestId != null) {
-      $result.requestId = requestId;
-    }
-    if (cancel != null) {
-      $result.cancel = cancel;
-    }
-    if (verbosity != null) {
-      $result.verbosity = verbosity;
-    }
-    if (sandboxDir != null) {
-      $result.sandboxDir = sandboxDir;
-    }
-    return $result;
+    final result = create();
+    if (arguments != null) result.arguments.addAll(arguments);
+    if (inputs != null) result.inputs.addAll(inputs);
+    if (requestId != null) result.requestId = requestId;
+    if (cancel != null) result.cancel = cancel;
+    if (verbosity != null) result.verbosity = verbosity;
+    if (sandboxDir != null) result.sandboxDir = sandboxDir;
+    return result;
   }
-  WorkRequest._() : super();
-  factory WorkRequest.fromBuffer($core.List<$core.int> i,
-          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromBuffer(i, r);
-  factory WorkRequest.fromJson($core.String i,
-          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromJson(i, r);
+
+  WorkRequest._();
+
+  factory WorkRequest.fromBuffer($core.List<$core.int> data,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(data, registry);
+  factory WorkRequest.fromJson($core.String json,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(json, registry);
 
   static final $pb.BuilderInfo _i = $pb.BuilderInfo(
       _omitMessageNames ? '' : 'WorkRequest',
       package: const $pb.PackageName(_omitMessageNames ? '' : 'blaze.worker'),
       createEmptyInstance: create)
     ..pPS(1, _omitFieldNames ? '' : 'arguments')
-    ..pc<Input>(2, _omitFieldNames ? '' : 'inputs', $pb.PbFieldType.PM,
-        subBuilder: Input.create)
-    ..a<$core.int>(3, _omitFieldNames ? '' : 'requestId', $pb.PbFieldType.O3)
+    ..pPM<Input>(2, _omitFieldNames ? '' : 'inputs', subBuilder: Input.create)
+    ..aI(3, _omitFieldNames ? '' : 'requestId')
     ..aOB(4, _omitFieldNames ? '' : 'cancel')
-    ..a<$core.int>(5, _omitFieldNames ? '' : 'verbosity', $pb.PbFieldType.O3)
+    ..aI(5, _omitFieldNames ? '' : 'verbosity')
     ..aOS(6, _omitFieldNames ? '' : 'sandboxDir')
     ..hasRequiredFields = false;
 
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-      'Will be removed in next major version')
-  WorkRequest clone() => WorkRequest()..mergeFromMessage(this);
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-      'Will be removed in next major version')
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
+  WorkRequest clone() => deepCopy();
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
   WorkRequest copyWith(void Function(WorkRequest) updates) =>
       super.copyWith((message) => updates(message as WorkRequest))
           as WorkRequest;
 
+  @$core.override
   $pb.BuilderInfo get info_ => _i;
 
   @$core.pragma('dart2js:noInline')
   static WorkRequest create() => WorkRequest._();
+  @$core.override
   WorkRequest createEmptyInstance() => create();
-  static $pb.PbList<WorkRequest> createRepeated() => $pb.PbList<WorkRequest>();
   @$core.pragma('dart2js:noInline')
   static WorkRequest getDefault() => _defaultInstance ??=
       $pb.GeneratedMessage.$_defaultFor<WorkRequest>(create);
   static WorkRequest? _defaultInstance;
 
   @$pb.TagNumber(1)
-  $core.List<$core.String> get arguments => $_getList(0);
+  $pb.PbList<$core.String> get arguments => $_getList(0);
 
   /// The inputs that the worker is allowed to read during execution of this
   /// request.
   @$pb.TagNumber(2)
-  $core.List<Input> get inputs => $_getList(1);
+  $pb.PbList<Input> get inputs => $_getList(1);
 
   /// Each WorkRequest must have either a unique
   /// request_id or request_id = 0. If request_id is 0, this WorkRequest must be
@@ -191,14 +169,11 @@ class WorkRequest extends $pb.GeneratedMessage {
   @$pb.TagNumber(3)
   $core.int get requestId => $_getIZ(2);
   @$pb.TagNumber(3)
-  set requestId($core.int v) {
-    $_setSignedInt32(2, v);
-  }
-
+  set requestId($core.int value) => $_setSignedInt32(2, value);
   @$pb.TagNumber(3)
   $core.bool hasRequestId() => $_has(2);
   @$pb.TagNumber(3)
-  void clearRequestId() => clearField(3);
+  void clearRequestId() => $_clearField(3);
 
   /// EXPERIMENTAL: When true, this is a cancel request, indicating that a
   /// previously sent WorkRequest with the same request_id should be cancelled.
@@ -206,14 +181,11 @@ class WorkRequest extends $pb.GeneratedMessage {
   @$pb.TagNumber(4)
   $core.bool get cancel => $_getBF(3);
   @$pb.TagNumber(4)
-  set cancel($core.bool v) {
-    $_setBool(3, v);
-  }
-
+  set cancel($core.bool value) => $_setBool(3, value);
   @$pb.TagNumber(4)
   $core.bool hasCancel() => $_has(3);
   @$pb.TagNumber(4)
-  void clearCancel() => clearField(4);
+  void clearCancel() => $_clearField(4);
 
   /// Values greater than 0 indicate that the worker may output extra debug
   /// information to stderr (which will go into the worker log). Setting the
@@ -221,14 +193,11 @@ class WorkRequest extends $pb.GeneratedMessage {
   @$pb.TagNumber(5)
   $core.int get verbosity => $_getIZ(4);
   @$pb.TagNumber(5)
-  set verbosity($core.int v) {
-    $_setSignedInt32(4, v);
-  }
-
+  set verbosity($core.int value) => $_setSignedInt32(4, value);
   @$pb.TagNumber(5)
   $core.bool hasVerbosity() => $_has(4);
   @$pb.TagNumber(5)
-  void clearVerbosity() => clearField(5);
+  void clearVerbosity() => $_clearField(5);
 
   /// The relative directory inside the workers working directory where the
   /// inputs and outputs are placed, for sandboxing purposes. For singleplex
@@ -242,14 +211,11 @@ class WorkRequest extends $pb.GeneratedMessage {
   @$pb.TagNumber(6)
   $core.String get sandboxDir => $_getSZ(5);
   @$pb.TagNumber(6)
-  set sandboxDir($core.String v) {
-    $_setString(5, v);
-  }
-
+  set sandboxDir($core.String value) => $_setString(5, value);
   @$pb.TagNumber(6)
   $core.bool hasSandboxDir() => $_has(5);
   @$pb.TagNumber(6)
-  void clearSandboxDir() => clearField(6);
+  void clearSandboxDir() => $_clearField(6);
 }
 
 /// The worker sends this message to Blaze when it finished its work on the
@@ -261,57 +227,47 @@ class WorkResponse extends $pb.GeneratedMessage {
     $core.int? requestId,
     $core.bool? wasCancelled,
   }) {
-    final $result = create();
-    if (exitCode != null) {
-      $result.exitCode = exitCode;
-    }
-    if (output != null) {
-      $result.output = output;
-    }
-    if (requestId != null) {
-      $result.requestId = requestId;
-    }
-    if (wasCancelled != null) {
-      $result.wasCancelled = wasCancelled;
-    }
-    return $result;
+    final result = create();
+    if (exitCode != null) result.exitCode = exitCode;
+    if (output != null) result.output = output;
+    if (requestId != null) result.requestId = requestId;
+    if (wasCancelled != null) result.wasCancelled = wasCancelled;
+    return result;
   }
-  WorkResponse._() : super();
-  factory WorkResponse.fromBuffer($core.List<$core.int> i,
-          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromBuffer(i, r);
-  factory WorkResponse.fromJson($core.String i,
-          [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
-      create()..mergeFromJson(i, r);
+
+  WorkResponse._();
+
+  factory WorkResponse.fromBuffer($core.List<$core.int> data,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromBuffer(data, registry);
+  factory WorkResponse.fromJson($core.String json,
+          [$pb.ExtensionRegistry registry = $pb.ExtensionRegistry.EMPTY]) =>
+      create()..mergeFromJson(json, registry);
 
   static final $pb.BuilderInfo _i = $pb.BuilderInfo(
       _omitMessageNames ? '' : 'WorkResponse',
       package: const $pb.PackageName(_omitMessageNames ? '' : 'blaze.worker'),
       createEmptyInstance: create)
-    ..a<$core.int>(1, _omitFieldNames ? '' : 'exitCode', $pb.PbFieldType.O3)
+    ..aI(1, _omitFieldNames ? '' : 'exitCode')
     ..aOS(2, _omitFieldNames ? '' : 'output')
-    ..a<$core.int>(3, _omitFieldNames ? '' : 'requestId', $pb.PbFieldType.O3)
+    ..aI(3, _omitFieldNames ? '' : 'requestId')
     ..aOB(4, _omitFieldNames ? '' : 'wasCancelled')
     ..hasRequiredFields = false;
 
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
-      'Will be removed in next major version')
-  WorkResponse clone() => WorkResponse()..mergeFromMessage(this);
-  @$core.Deprecated('Using this can add significant overhead to your binary. '
-      'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
-      'Will be removed in next major version')
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
+  WorkResponse clone() => deepCopy();
+  @$core.Deprecated('See https://github.com/google/protobuf.dart/issues/998.')
   WorkResponse copyWith(void Function(WorkResponse) updates) =>
       super.copyWith((message) => updates(message as WorkResponse))
           as WorkResponse;
 
+  @$core.override
   $pb.BuilderInfo get info_ => _i;
 
   @$core.pragma('dart2js:noInline')
   static WorkResponse create() => WorkResponse._();
+  @$core.override
   WorkResponse createEmptyInstance() => create();
-  static $pb.PbList<WorkResponse> createRepeated() =>
-      $pb.PbList<WorkResponse>();
   @$core.pragma('dart2js:noInline')
   static WorkResponse getDefault() => _defaultInstance ??=
       $pb.GeneratedMessage.$_defaultFor<WorkResponse>(create);
@@ -320,29 +276,25 @@ class WorkResponse extends $pb.GeneratedMessage {
   @$pb.TagNumber(1)
   $core.int get exitCode => $_getIZ(0);
   @$pb.TagNumber(1)
-  set exitCode($core.int v) {
-    $_setSignedInt32(0, v);
-  }
-
+  set exitCode($core.int value) => $_setSignedInt32(0, value);
   @$pb.TagNumber(1)
   $core.bool hasExitCode() => $_has(0);
   @$pb.TagNumber(1)
-  void clearExitCode() => clearField(1);
+  void clearExitCode() => $_clearField(1);
 
-  /// This is printed to the user after the WorkResponse has been received and is
-  /// supposed to contain compiler warnings / errors etc. - thus we'll use a
-  /// string type here, which gives us UTF-8 encoding.
+  /// Output message for this work unit.
+  /// This is akin to the combined stdout/stderr if the work unit were executed
+  /// as a standalone process. Output pertaining to a work unit should be
+  /// reported here instead of through the stdout/stderr of the worker process.
+  /// Assumed to be UTF-8 encoded.
   @$pb.TagNumber(2)
   $core.String get output => $_getSZ(1);
   @$pb.TagNumber(2)
-  set output($core.String v) {
-    $_setString(1, v);
-  }
-
+  set output($core.String value) => $_setString(1, value);
   @$pb.TagNumber(2)
   $core.bool hasOutput() => $_has(1);
   @$pb.TagNumber(2)
-  void clearOutput() => clearField(2);
+  void clearOutput() => $_clearField(2);
 
   /// This field must be set to the same request_id as the WorkRequest it is a
   /// response to. Since worker processes which support multiplex worker will
@@ -351,14 +303,11 @@ class WorkResponse extends $pb.GeneratedMessage {
   @$pb.TagNumber(3)
   $core.int get requestId => $_getIZ(2);
   @$pb.TagNumber(3)
-  set requestId($core.int v) {
-    $_setSignedInt32(2, v);
-  }
-
+  set requestId($core.int value) => $_setSignedInt32(2, value);
   @$pb.TagNumber(3)
   $core.bool hasRequestId() => $_has(2);
   @$pb.TagNumber(3)
-  void clearRequestId() => clearField(3);
+  void clearRequestId() => $_clearField(3);
 
   /// EXPERIMENTAL When true, indicates that this response was sent due to
   /// receiving a cancel request. The exit_code and output fields should be empty
@@ -369,16 +318,14 @@ class WorkResponse extends $pb.GeneratedMessage {
   @$pb.TagNumber(4)
   $core.bool get wasCancelled => $_getBF(3);
   @$pb.TagNumber(4)
-  set wasCancelled($core.bool v) {
-    $_setBool(3, v);
-  }
-
+  set wasCancelled($core.bool value) => $_setBool(3, value);
   @$pb.TagNumber(4)
   $core.bool hasWasCancelled() => $_has(3);
   @$pb.TagNumber(4)
-  void clearWasCancelled() => clearField(4);
+  void clearWasCancelled() => $_clearField(4);
 }
 
-const _omitFieldNames = $core.bool.fromEnvironment('protobuf.omit_field_names');
-const _omitMessageNames =
+const $core.bool _omitFieldNames =
+    $core.bool.fromEnvironment('protobuf.omit_field_names');
+const $core.bool _omitMessageNames =
     $core.bool.fromEnvironment('protobuf.omit_message_names');
