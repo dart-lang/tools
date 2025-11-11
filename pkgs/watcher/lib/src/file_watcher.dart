f0467d754f3d0cda3c64ed3a94979f4f565becd4
Morgan :) <davidmorgan@google.com>
2025-11-11 09:43:14 +0100

    Document Linux watcher exhaustion error. (#2243)

--- pkgs/watcher/lib/src/file_watcher.dart
+++ pkgs/watcher/lib/src/file_watcher.dart
@@ -16,6 +16,11 @@ import 'file_watcher/native.dart';
 ///
 /// If the file is deleted and quickly replaced (when a new file is moved in its
 /// place, for example) this will emit a [ChangeType.MODIFY] event.
+///
+/// On Linux, the underlying SDK `File.watch` fails if the system limit on
+/// watchers has been reached. If this happens the SDK exception is thrown, it
+/// is a `FileSystemException` with message `Failed to watch path` and
+/// OSError `No space left on device`, `errorCode = 28`.
 abstract class FileWatcher implements Watcher {
   /// Creates a new [FileWatcher] monitoring [file].
   ///
