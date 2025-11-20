77f592be4b97106d3651b658f9d8c259fbb06b5a
David Morgan <davidmorgan@google.com>
2025-11-20 09:48:25 +0100

    Add way to run e2e test as a standalone binary.

--- pkgs/watcher/test/directory_watcher/client_simulator.dart
+++ pkgs/watcher/test/directory_watcher/client_simulator.dart
@@ -5,7 +5,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:test/test.dart';
 import 'package:watcher/watcher.dart';
 
 /// Simulates a typical use case for `package:watcher`.
@@ -16,6 +15,7 @@ import 'package:watcher/watcher.dart';
 /// lengths on disk.
 class ClientSimulator {
   final Watcher watcher;
+  final void Function(String) printOnFailure;
 
   /// Events and actions, for logging on failure.
   final List<String> messages = [];
@@ -25,14 +25,17 @@ class ClientSimulator {
   StreamSubscription<WatchEvent>? _subscription;
   DateTime _lastEventAt = DateTime.now();
 
-  ClientSimulator._(this.watcher);
+  ClientSimulator._({required this.watcher, required this.printOnFailure});
 
   /// Creates a `ClientSimulator` watching with [watcher].
   ///
   /// When returned, it has already read the filesystem state and started
   /// tracking file lengths using watcher events.
-  static Future<ClientSimulator> watch(Watcher watcher) async {
-    final result = ClientSimulator._(watcher);
+  static Future<ClientSimulator> watch(
+      {required Watcher watcher,
+      required void Function(String) printOnFailure}) async {
+    final result =
+        ClientSimulator._(watcher: watcher, printOnFailure: printOnFailure);
     result._initialRead();
     result._subscription = watcher.events.listen(result._handleEvent);
     await watcher.ready;
@@ -121,9 +124,9 @@ class ClientSimulator {
 
   /// Returns whether tracked state matches actual state on disk.
   ///
-  /// If not, and [log] is `true`, prints an explanation of the difference
-  /// with `printOnFailure`.
-  bool verify({required bool log}) {
+  /// If not, and [printOnFailure] is passed, uses it to print a dscription of
+  /// the failure.
+  bool verify({void Function(String)? printOnFailure}) {
     final fileLengths = _readFileLengths();
 
     var result = true;
@@ -133,7 +136,7 @@ class ClientSimulator {
     if (unexpectedFiles.isNotEmpty) {
       result = false;
 
-      if (log) {
+      if (printOnFailure != null) {
         printOnFailure('Failed, on disk but not tracked:');
         printOnFailure(
             unexpectedFiles.map((path) => path.padLeft(4)).join('\n'));
@@ -144,7 +147,7 @@ class ClientSimulator {
         _trackedFileLengths.keys.toSet().difference(fileLengths.keys.toSet());
     if (missingExpectedFiles.isNotEmpty) {
       result = false;
-      if (log) {
+      if (printOnFailure != null) {
         printOnFailure('Failed, tracked but not on disk:');
         printOnFailure(
             missingExpectedFiles.map((path) => path.padLeft(4)).join('\n'));
@@ -160,7 +163,7 @@ class ClientSimulator {
     }
     if (differentFiles.isNotEmpty) {
       result = false;
-      if (log) {
+      if (printOnFailure != null) {
         printOnFailure('Failed, tracking is out of date:');
         final output = StringBuffer();
         for (final path in differentFiles) {
