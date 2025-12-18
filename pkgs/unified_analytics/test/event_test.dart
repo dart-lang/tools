3f0e38bcef060a5667d9a86582d4298c86637b96
Sam Rawlins <srawlins@google.com>
2025-12-18 11:57:12 -0800

    one-more

--- pkgs/unified_analytics/test/event_test.dart
+++ pkgs/unified_analytics/test/event_test.dart
@@ -827,7 +827,7 @@ void main() {
 
     // Change this integer below if your PR either adds or removes
     // an Event constructor
-    final eventsAccountedForInTests = 32;
+    final eventsAccountedForInTests = 33;
     expect(eventsAccountedForInTests, constructorCount,
         reason: 'If you added or removed an event constructor, '
             'ensure you have updated '
