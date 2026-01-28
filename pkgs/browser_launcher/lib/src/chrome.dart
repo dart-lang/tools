bf111b688b85a4adf3dccbedcc1bafe796e84925
Nate Bosch <nbosch@google.com>
2026-01-28 23:39:27 +0000

    Add --test-type flag
    Closes #2300
    
    Some flags like disabling `TimerThrottlingForBackgroundTabs` cause
    warnings when launching chrome because it is not recommended for use
    during general browsing. Suppress these types of warnings with the
    `--test-type` flag which indicates to chrome that this is a dev focused
    workflow where such flags are expected.

--- pkgs/browser_launcher/lib/src/chrome.dart
+++ pkgs/browser_launcher/lib/src/chrome.dart
@@ -118,6 +118,9 @@ class Chrome {
       // credentials. This uses a mock keychain to avoid that dialog from
       // blocking.
       '--use-mock-keychain',
+      // Prevent warnings for using flags that are disrecommended for general
+      // browsing but applicable for use in dev focused workflows.
+      '--test-type',
     ];
     if (headless) {
       args.add('--headless');
