27d3ceb09be42226a93ac1b86c090f5c9aa41f7d
kevmoo <kevmoo@google.com>
2026-03-10 14:45:33 -0700

    Address PR 2342 comments
    - Refactor CR+LF handling for readability
    - Migrate benchmark to benchmark_harness
    - Add missing license header

--- pkgs/string_scanner/benchmark/benchmark.dart
+++ pkgs/string_scanner/benchmark/benchmark.dart
@@ -1,76 +1,104 @@
+// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:string_scanner/string_scanner.dart';
 
-void main() {
-  final string = 'This is a test string with some typical content.\n' * 50000;
+final _string = 'This is a test string with some typical content.\n' * 50000;
+final _word = RegExp(r'\w+');
+final _space = RegExp(r'\s+');
 
-  print('String length: ${string.length}');
+class StringScannerReadCharBenchmark extends BenchmarkBase {
+  StringScannerReadCharBenchmark() : super('StringScanner readChar');
 
-  void runBenchmark(String name, void Function() fn) {
-    // Warmup
-    for (var i = 0; i < 5; i++) {
-      fn();
+  @override
+  void run() {
+    final scanner = StringScanner(_string);
+    while (!scanner.isDone) {
+      scanner.readChar();
     }
-
-    // Measure
-    final watch = Stopwatch()..start();
-    for (var i = 0; i < 10; i++) {
-      fn();
-    }
-    watch.stop();
-    print('$name: ${watch.elapsedMilliseconds / 10} ms/iter');
   }
+}
 
-  runBenchmark('StringScanner readChar', () {
-    final scanner = StringScanner(string);
+class LineScannerReadCharBenchmark extends BenchmarkBase {
+  LineScannerReadCharBenchmark() : super('LineScanner readChar');
+
+  @override
+  void run() {
+    final scanner = LineScanner(_string);
     while (!scanner.isDone) {
       scanner.readChar();
     }
-  });
+  }
+}
 
-  runBenchmark('LineScanner readChar', () {
-    final scanner = LineScanner(string);
+class SpanScannerReadCharBenchmark extends BenchmarkBase {
+  SpanScannerReadCharBenchmark() : super('SpanScanner readChar');
+
+  @override
+  void run() {
+    final scanner = SpanScanner(_string);
     while (!scanner.isDone) {
       scanner.readChar();
     }
-  });
+  }
+}
 
-  runBenchmark('SpanScanner readChar', () {
-    final scanner = SpanScanner(string);
+class StringScannerScanBenchmark extends BenchmarkBase {
+  StringScannerScanBenchmark() : super('StringScanner scan');
+
+  @override
+  void run() {
+    final scanner = StringScanner(_string);
     while (!scanner.isDone) {
-      scanner.readChar();
-    }
-  });
-
-  final word = RegExp(r'\w+');
-  final space = RegExp(r'\s+');
-
-  runBenchmark('StringScanner scan', () {
-    final scanner = StringScanner(string);
-    while (!scanner.isDone) {
-      if (!scanner.scan(word)) {
-        scanner.scanChar(10); // \n
-        scanner.scan(space);
+      if (!scanner.scan(_word) &&
+          !scanner.scanChar(10) &&
+          !scanner.scan(_space)) {
+        scanner.readChar();
       }
     }
-  });
+  }
+}
 
-  runBenchmark('LineScanner scan', () {
-    final scanner = LineScanner(string);
+class LineScannerScanBenchmark extends BenchmarkBase {
+  LineScannerScanBenchmark() : super('LineScanner scan');
+
+  @override
+  void run() {
+    final scanner = LineScanner(_string);
     while (!scanner.isDone) {
-      if (!scanner.scan(word)) {
-        scanner.scanChar(10);
-        scanner.scan(space);
+      if (!scanner.scan(_word) &&
+          !scanner.scanChar(10) &&
+          !scanner.scan(_space)) {
+        scanner.readChar();
       }
     }
-  });
+  }
+}
 
-  runBenchmark('SpanScanner scan', () {
-    final scanner = SpanScanner(string);
+class SpanScannerScanBenchmark extends BenchmarkBase {
+  SpanScannerScanBenchmark() : super('SpanScanner scan');
+
+  @override
+  void run() {
+    final scanner = SpanScanner(_string);
     while (!scanner.isDone) {
-      if (!scanner.scan(word)) {
-        scanner.scanChar(10);
-        scanner.scan(space);
+      if (!scanner.scan(_word) &&
+          !scanner.scanChar(10) &&
+          !scanner.scan(_space)) {
+        scanner.readChar();
       }
     }
-  });
+  }
+}
+
+void main() {
+  print('String length: ${_string.length}');
+  StringScannerReadCharBenchmark().report();
+  LineScannerReadCharBenchmark().report();
+  SpanScannerReadCharBenchmark().report();
+  StringScannerScanBenchmark().report();
+  LineScannerScanBenchmark().report();
+  SpanScannerScanBenchmark().report();
 }
