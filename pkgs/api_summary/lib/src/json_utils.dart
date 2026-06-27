f7fffea518e850729c2838bf54de033826ec34d6
kevmoo <kevmoo@google.com>
2026-06-27 14:12:29 -0700

    refactor(api_summary): introduce structured API declaration and type models

--- pkgs/api_summary/lib/src/json_utils.dart
+++ pkgs/api_summary/lib/src/json_utils.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'api_type.dart';
+
 /// Parses a JSON list of objects using [fromJson].
 List<T> parseList<T>(
   Map<String, dynamic> json,
@@ -16,3 +18,18 @@ List<T> parseList<T>(
 /// Parses a JSON list of strings.
 List<String> parseStringList(Map<String, dynamic> json, String key) =>
     (json[key] as List<dynamic>?)?.map((e) => e as String).toList() ?? [];
+
+/// Parses a JSON map representing type parameters and their bounds.
+Map<String, ApiType?> parseTypeParameters(
+  Map<String, dynamic> json,
+  String key,
+) {
+  final map = json[key];
+  if (map is! Map) return {};
+  return {
+    for (final entry in map.entries)
+      entry.key as String: entry.value == null
+          ? null
+          : ApiType.fromJson(entry.value as Map<String, dynamic>),
+  };
+}
