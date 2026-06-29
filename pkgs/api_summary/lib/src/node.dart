95c33d304afd920f04a218651590c0e2334e76bf
kevmoo <kevmoo@google.com>
2026-06-29 11:48:45 -0700

    refactor(api_summary): Revamp API summary generation architecture

--- pkgs/api_summary/lib/src/node.dart
+++ pkgs/api_summary/lib/src/node.dart
@@ -2,9 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// @docImport 'unique_namer.dart';
-library;
-
 import 'package:collection/collection.dart';
 
 /// Outputs the contents of [nodes] to [sink], prepending [prefix] to every
@@ -14,21 +11,20 @@ void printNodes<SortKey extends Comparable<SortKey>>(
   List<(SortKey, Node)> nodes, {
   String prefix = '',
 }) {
-  for (final entry in nodes.sortedBy((n) => n.$1)) {
-    final node = entry.$2;
+  for (final (_, node) in nodes.sortedBy((n) => n.$1)) {
     sink.writeln('$prefix${node.text.join()}');
     node.printChildren(sink, prefix: '$prefix  ');
   }
 }
 
 /// A node to be printed to the output.
-class Node<ChildSortKey extends Comparable<ChildSortKey>> {
+final class Node<ChildSortKey extends Comparable<ChildSortKey>> {
   /// A list of objects which, when their string representations are
   /// concatenated, is the text that should be displayed on the first line of
   /// the node.
   ///
   /// The reason this is a list rather than a single string is to allow elements
-  /// of the list to be [UniqueName] objects, which may acquire a disambiguation
+  /// of the list to be unique name objects, which may acquire a disambiguation
   /// suffix at a later time.
   final text = <Object?>[];
 
