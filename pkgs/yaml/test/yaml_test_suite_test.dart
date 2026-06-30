dee887175cf0e799a9e35e6be2efad4f021dc5d5
Jonas Finnemann Jensen <jonasfj@google.com>
2026-06-30 11:32:10 +0200

    Integrate `yaml-test-suite` for specification compliance testing (#2441)

---
+++ pkgs/yaml/test/yaml_test_suite_test.dart
@@ -0,0 +1,82 @@
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+@TestOn('vm')
+library;
+
+import 'package:test/test.dart';
+
+import 'yaml_test_suite.dart';
+
+void main() {
+  final cases = loadYamlTestSuite();
+
+  for (final c in cases) {
+    test(c.name, () {
+      if (_expectedFailures.contains(c.id)) {
+        try {
+          c.runTest();
+        } on TestFailure {
+          return; // Expected failure, exit the test successfully.
+        } catch (e) {
+          return; // Other errors from package:yaml are also expected failures.
+        }
+        fail(
+          'Test ${c.id} was expected to fail, but it passed. '
+          'Please remove it from _expectedFailures!',
+        );
+      } else {
+        c.runTest();
+      }
+    });
+  }
+}
+
+const _expectedFailures = <String>[
+  'VJP3-0',
+  '7FWL-0',
+  '565N-0',
+  '6CK3-0',
+  'J7PZ-0',
+  'UGM3-0',
+  'X4QW-0',
+  'CC74-0',
+  'CVW2-0',
+  'QB6E-0',
+  'SU5Z-0',
+  'DK4H-0',
+  '5TYM-0',
+  'Y79Y-0',
+  'Y79Y-3',
+  'Y79Y-4',
+  'Y79Y-5',
+  'Y79Y-6',
+  'Y79Y-7',
+  'Y79Y-8',
+  'Y79Y-9',
+  '9C9N-0',
+  'DK95-0',
+  'DK95-1',
+  'DK95-4',
+  'LHL4-0',
+  'ZXT5-0',
+  'L24T-1',
+  'P76L-0',
+  '9JBA-0',
+  'S98Z-0',
+  '2JQS-0',
+  'MUS6-0',
+  'MUS6-1',
+  'BS4K-0',
+  'X38W-0',
+  'ZYU8-2',
+  'RHX7-0',
+  'UKK6-1',
+  '2XXW-0',
+  '9HCY-0',
+  'KS4U-0',
+  'Z9M4-0',
+  'EB22-0',
+  '3HFZ-0',
+];
