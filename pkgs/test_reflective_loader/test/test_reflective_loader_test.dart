28396db2a29c45bedcfdaf45862821aaf248bdb9
Moritz <27859082+mosuem@users.noreply.github.com>
2026-08-03 09:34:24 +0200

    [test_reflective_loader] Ignore unreachable_from_main in test files (#2486)

--- pkgs/test_reflective_loader/test/test_reflective_loader_test.dart
+++ pkgs/test_reflective_loader/test/test_reflective_loader_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, unreachable_from_main
 
 import 'dart:async';
 
